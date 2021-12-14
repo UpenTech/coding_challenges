027ef7d3fc2be56f0e37500669cfba1727c296d7
ndeu <gautamupen99@gmail.com>
2021-12-14 23:09:37 +0545

    Codefile changes

--- codeFile.py
+++ codeFile.py
@@ -10,7 +10,9 @@ class Database:
 		print("\n" * 8)
 		print("\t" *4, end = "")
 		print("<    " + Fore.YELLOW + "CENTRAL DATABASE OF XYZ CAMPUS" + Fore.RESET + "    >")
-
+		print("\n" *2)
+		print("\t" * 5, end = "")
+		print("Secret Code: ", end = "\n" * 8)
 
 Data = Database("A")
 Data.startupWindow()
