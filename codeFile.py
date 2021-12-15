8d0be27e8351d3cc8c4570450066312d1ca36111
ndeu <gautamupen99@gmail.com>
2021-12-16 00:08:47 +0545

    ...

--- codeFile.py
+++ codeFile.py
@@ -5,12 +5,14 @@ window = Tk()
 # Title-Bar settings and resolution
 window.title("Database Accesser")
 window.geometry('800x400')
+window.configure(background="#0e3939")
 
+img = PhotoImage(file="logo.jpg")
+window.iconphoto(False, img)
 
 inital_page = Label(
 	text="Welcome to this database",
-	foreground="#3a7b7b",
-	background="#0e3939")
+	foreground="#3a7b7b")
 
 
 inital_page.pack()
