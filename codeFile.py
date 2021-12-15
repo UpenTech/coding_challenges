56555dac7a63c67361f668de1beb78576f552287
ndeu <gautamupen99@gmail.com>
2021-12-16 00:39:38 +0545

    ....

--- codeFile.py
+++ codeFile.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import font
 
 # Create Window
 window = Tk()
@@ -7,12 +8,15 @@ window.title("Database Accesser")
 window.geometry('800x400')
 window.configure(background="#0e3939")
 
-img = PhotoImage(file="logo.jpg")
+img = PhotoImage(file="A:\\LearnPython\\CodingChallenges\\logo.png")
 window.iconphoto(False, img)
 
 inital_page = Label(
 	text="Welcome to this database",
-	foreground="#3a7b7b")
+	foreground="#085E43",
+	background="#0e3939")
+
+inital_page.config(font=('Copperplate Gothic Light', 25))
 
 
 inital_page.pack()
