aa1af87d307326b78914cf6776bad99dcfdd9bcf
ndeu <gautamupen99@gmail.com>
2021-12-15 23:12:08 +0545

    Fresh Start

--- codeFile.py
+++ codeFile.py
@@ -1,18 +1,12 @@
-from colorama import Fore
+from tkinter import *
 
-class Database:
+window = Tk()
+inital_page = Label(
+	text="Welcome to this database",
+	foreground="Green",
+	background="Yellow")
 
-	def __init__(self, loc):
-		self.loc = loc
+	
+inital_page.pack()
 
-	def startupWindow(self):
-
-		print("\n" * 8)
-		print("\t" *4, end = "")
-		print("<    " + Fore.YELLOW + "CENTRAL DATABASE OF XYZ CAMPUS" + Fore.RESET + "    >")
-		print("\n" *2)
-		print("\t" * 5, end = "")
-		print("Secret Code: ", end = "\n" * 8)
-
-Data = Database("A")
-Data.startupWindow()
+window.mainloop()
